4dbc9d8aad43d1cd98a21103887c89e68ee2c69a
RyanHunter-DV <86789670+RyanHunter-DV@users.noreply.github.com>
2022-03-07 10:07:56 +0800

    Update clockGen_uvc.svh

--- clockGen/clockGen_uvc.svh
+++ clockGen/clockGen_uvc.svh
@@ -8,10 +8,13 @@ class clockGen_uvc extends uvm_agent; // {
 	clockGen_seqr    seqr;
 	clockGen_config  cfg;
 
+	`uvm_component_utils_begin(clockGen_uvc)
+	`uvm_component_utils_end
+	
 	function new (string name="clockGen_uvc",uvm_component parent=null);
 		super.new(name,parent);
 		// create config table
-		cfg = clockGen_config::type_id::create("cfg",this);
+		cfg = clockGen_config::type_id::create("cfg");
 	endfunction
 
 	// phases
@@ -24,9 +27,14 @@ class clockGen_uvc extends uvm_agent; // {
 	extern function void setFreq(string name,real Freq);
 	extern function void setJitter(string name,real Jitter);
 	extern function void setSkew(string name,real Skew);
-	extern function void reset();
+	extern function void setInterfacePath(string name);
+	// currently not supported, extern function void reset();	
 
 endclass // }
+
+function void clockGen_uvc::setInterfacePath(string name);
+	cfg.intfPath = name;
+endfunction
 		
 function void clockGen_uvc::addClock(string name,real freq,real skew=0,real jitter=0); // {
 	cfg.addClock(name,freq,skew,jitter);
@@ -56,8 +64,10 @@ function void clockGen_uvc::build_phase (uvm_phase phase); // {
 	if (is_active==UVM_ACTIVE) begin // {
 		drv = clockGen_driver::type_id::create("drv",this);
 		seqr = clockGen_seqr::type_id::create("seqr",this);
+		drv.cfg=cfg;
 	end // }
 	mon = clockGen_monitor::type_id::create("mon",this);
+	mon.cfg=cfg;
 
 	// TODO
 endfunction // }
@@ -66,5 +76,10 @@ function void clockGen_uvc::connect_phase(uvm_phase phase); // {
 	if (is_active==UVM_ACTIVE)
 		drv.seq_item_port.connect(seqr.seq_item_export);
 endfunction // }
+
+function void clockGen_uvc::end_of_elaboration_phase(uvm_phase phase); // {
+	cfg.elaborateConfigs;
+endfunction
+
 
 `endif
