830e119b735eee8fc557e44dbe628b190eb0732b
RyanHunter-DV <86789670+RyanHunter-DV@users.noreply.github.com>
2023-01-13 15:50:19 +0800

    tiny fix

--- gpv/src/rhGpvIf.sv
+++ gpv/src/rhGpvIf.sv
@@ -4,6 +4,7 @@
 interface RhGpvIf();
 	logic [`RHGPV_MAX_VECTOR_WIDTH-1:0] vector;
 	logic [`RHGPV_MAX_CLOCK_WIDTH-1:0] clock;
+	logic [`RHGPV_MAX_RESET_WIDTH-1:0] reset;
 
 
 	function void driveVector(int spos,logic bits[]);
--- gpv/src/rhGpvTypes.svh
+++ gpv/src/rhGpvTypes.svh
@@ -3,6 +3,7 @@
 
 `define RHGPV_MAX_VECTOR_WIDTH 1024
 `define RHGPV_MAX_CLOCK_WIDTH 16
+`define RHGPV_MAX_RESET_WIDTH 16
 
 
 typedef bit [`RHGPV_MAX_VECTOR_WIDTH-1:0] bitVector_t;
